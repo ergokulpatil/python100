f6e1493cee473cd668d54bd14da4f7527c7ceaef
ergokulpatil <glp.gokulpatil@gmail.com>
2024-01-29 15:19:38 +0530

    day-1 code commited

---
+++ sec1/day1.py
@@ -0,0 +1,33 @@
+# print("Welcome to python code")
+# print("print('what to print')")
+
+#some string manipulation
+# print("Hello world\nHello World\n")
+#string concatenation
+# print("Hello" + " Gokul")
+# print(input("What is your name?"))
+
+#take input is string and count the number of char in string
+# x=input("Enter the string-->")
+# print(len(x))
+
+#swap two number 
+# a= input()
+# b= input()
+# print("Before swaping: " , a,b )
+# a,b=b,a
+# print("After swaping: " , a,b )
+
+
+#band name generator
+print("welcome to band generator program")
+user_city=input("Enter city name")
+pet_name=input("Enter your pet_name")
+print("The user band city "+user_city+" pet name " +pet_name)
+
+
+
+
+
+
+
