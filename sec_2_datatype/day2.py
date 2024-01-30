f2a243efa7787afdc86366c4f8f95fa331ac4e45
ergokulpatil <glp.gokulpatil@gmail.com>
2024-01-30 11:02:40 +0530

    day2 data uploaded

---
+++ sec_2_datatype/day2.py
@@ -0,0 +1,46 @@
+#string
+# print("Hello"[0])
+# print("123"+"456")
+
+#integer
+# x=int(input("Enter the number"))
+# print(x)
+#print(12+34)
+
+#float
+#3.1233
+
+#boolean
+#true or false
+
+
+#function or data type using type() function
+# a=float(123)
+# print(type(a))
+
+#write program that add the digit in 2 digit number
+#if the input was 35 then the output should be 3+5=8
+# two_digit_number=input()
+# #print(type(two_digit_number))
+# print(int(two_digit_number[0]) +int(two_digit_number[1]))
+
+# print(3+5)
+# print(5-1)
+# print(3*2)
+# print(6/3)
+# print(2**3)
+# print(6//3)
+
+# print(3*3+3/3-3)
+
+
+#BMI calculator
+
+# height=input("Enter height of person")
+# weight=input("Enter weight of person")
+# bmi=float(weight) /(float(height) **2)
+# print(bmi)
+
+#use of f in the string printing
+score=0
+print(f"Your score is:{score}")
